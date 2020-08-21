2e39b1f43fe16be75474ca27f965de7ee4b175e9
Sleggi <soltankirill23@gmail.com>
2020-08-21 09:29:32 +0300

    Setting up the project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAq9kbqaMblD5iWibsE4oKnY1dNr4TJ8UM",
+    authDomain: "chat-app-62224.firebaseapp.com",
+    databaseURL: "https://chat-app-62224.firebaseio.com",
+    projectId: "chat-app-62224",
+    storageBucket: "chat-app-62224.appspot.com",
+    messagingSenderId: "885355981350",
+    appId: "1:885355981350:web:39e1b70e32352a44aa94e7",
+    measurementId: "G-VMBT9QMRW8"
+};
