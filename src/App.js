c3c71c795604bc5cd60db5cc78722cc4444ae754
Sleggi <soltankirill23@gmail.com>
2020-08-21 09:55:59 +0300

    installing firebase and materialUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        {/* chat */}
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './sidebar.css'
+
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header"></div>
+            <div className="sidebar__search"></div>
+            <div className="sidebar__chats"></div>
+        </div>
+    );
+}
+
+export default Sidebar;
